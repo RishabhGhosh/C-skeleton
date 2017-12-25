228a3305c35d597156bf777c15ed94017dd29af4
Rishabh Ghosh <rishabhghosh81@gmail.com>
2017-12-25 23:05:12 +0530

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -3,32 +3,14 @@
 #include "bits/stdc++.h"
 
 #define ll long long
-#define vi  vector <int>
-#define vs  vector <string>
-#define vll  vector <long long>
-#define vpii vector <pair <int, int> >
-#define vpss vector <pair <string, string> >
-#define vpll vector <pair <long long, long long> >
-
-#define pii pair <int,int>
-#define pll pair <long long, long long>
-#define pss pair <string, string>
-
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
-
-#define fi(i,a,b,c) for(int i=a; i<b; i+=c)
 #define fl(i,a,b,c) for(long long i=a; i<b; i+=c)
-#define rfi(i,a,b,c) for(int i=a; i>=b; i-=c)
 #define rfl(i,a,b,c) for(long long i=a; i>=b; i-=c)
 
 #define pb  push_back
-#define mk  make_pair
-#define ep  emplace_back
 #define F first
 #define S second
-#define sc scanf
-#define pr printf
 
 using namespace std;
 
@@ -54,7 +36,3 @@ const int MAXN = 1e6 + 5;
 
 //void solve() {
 //} int main() { ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); solve(); return 0; }
-
-// Powered by KawigiEdit 2.1.4 (beta) modified by pivanof! [Topcoder] (Appirio, Wipro Ltd.) 
-// Powered by Greed 2.0-RC [Topcoder] (Appirio, Wipro Ltd.) 
-// SCE powered by JavaScript and Chromium (GitHub Inc.)
