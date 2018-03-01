bce789e016be412d0530fa86a0a762417cd53cae
Rishabh Ghosh <rishabhghosh81@gmail.com>
2018-03-01 21:35:55 +0530

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -1,44 +1,31 @@
-/* realRPG | India */
-
-#include "bits/stdc++.h"
-
+/*
+    author  : real_rpg  
+    country : India
+*/
+ 
+#include "bits/stdc++.h" 
 #define ll long long
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
-#define rep(i,n) for(long long i=0; i<n;i++)
-#define fl(i,a,b,c) for(long long i=a; i<b; i+=c)
-#define rfl(i,a,b,c) for(long long i=a; i>=b; i-=c)
-
+#define fl(i,a,b,c) for(ll i=a; i<b; i+=c)
+#define rfl(i,a,b,c) for(ll i=a; i>=b; i-=c) 
+#define rep(i,n) fl(i,0,n,1)
+#define rrep(i,n) rfl(i,n-1,0,1)
+#define sas int tt; cin >> tt; rep(tc,tt) 
 #define pb  push_back
 #define F first
 #define S second
-
-using namespace std;
-
+using namespace std; 
 const int md = 1e9 + 7;
 const int MAX = 1e5 + 5;
 const int MAXN = 1e6 + 5;
 
-/*inline int fsc() {
-        int ip = getchar(), ret = 0, flag = 1;
-        for (; ip<'0' || ip>'9'; ip = getchar_unlocked())
-                if (ip == '-') {
-                        flag = -1; ip = getchar_unlocked();
-                        break;
-                }
-        for (; ip >= '0'&&ip <= '9'; ip = getchar_unlocked())
-                ret = ret * 10 + ip - '0';
-        return flag*ret;
-}*/
-
-//class { public: };
-
-/*void solve() {
-} 
+void solve() {
+}
 
 int main() { 
         ios_base::sync_with_stdio(false); 
         cin.tie(NULL); cout.tie(NULL); 
         solve(); 
         return 0; 
-}*/
+}
