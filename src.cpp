2c05f612a1b9c6ad8069b11570beffe77640d7d8
Rishabh Ghosh <rishabhghosh81@gmail.com>
2018-07-26 19:16:54 +0530

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -1,18 +1,22 @@
 /*
     author  : real_rpg  
     country : India
+    time    : 00:51:12 IST
 */
  
 #include "bits/stdc++.h" 
 #define ll long long
+#define ld long double
 #define all(a) a.begin(), a.end()
 #define rall(a) a.rbegin(), a.rend()
+#define fld(i,a,b,c) for(ld i=a; i<b; i+=c)
 #define fl(i,a,b,c) for(ll i=a; i<b; i+=c)
 #define rfl(i,a,b,c) for(ll i=a; i>=b; i-=c) 
 #define rep(i,n) fl(i,0,n,1)
 #define rrep(i,n) rfl(i,n-1,0,1)
 #define sas int tt; cin >> tt; rep(tc,tt) 
 #define pb  push_back
+#define ep emplace_back
 #define F first
 #define S second
 using namespace std; 
@@ -20,12 +24,12 @@ const int md = 1e9 + 7;
 const int MAX = 1e5 + 5;
 const int MAXN = 1e6 + 5;
 
-void solve() {
+void solve() {  
 }
 
 int main() { 
-        ios_base::sync_with_stdio(false); 
-        cin.tie(NULL); cout.tie(NULL); 
-        solve(); 
-        return 0; 
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); cout.tie(NULL); 
+    solve(); 
+    return 0; 
 }
